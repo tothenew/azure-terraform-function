0b7455391499884f2284f37f09485069faeda17d
gaurav11 <gaurav1@tothenew.com>
2024-04-02 15:37:37 +0530

    updating data block

--- _data.tf
+++ _data.tf
@@ -6,6 +6,7 @@ data "azurerm_resource_group" "rg" {
 
 
 data "azurerm_storage_account" "storage_acc" {
+  count               = var.create_storage_account ? 0: 1
   name                = var.storage_account_name
   resource_group_name = var.storage_account_resource_group_name
 }
