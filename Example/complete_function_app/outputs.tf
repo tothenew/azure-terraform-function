f70e5ac5e58d5281f31f32a5189b2a4584cf94b0
gaurav11 <gaurav1@tothenew.com>
2024-04-08 12:58:00 +0530

    update  output.tf

--- Example/complete_function_app/outputs.tf
+++ Example/complete_function_app/outputs.tf
@@ -1,15 +1,11 @@
 output "linux_function_app_default_hostnames" {
   description = "The Default Hostnames associated with the Azure Function Apps for Linux OS"
-  value = os_type == "Linux" ? {
-    for function_app_key, function_app in azurerm_linux_function_app.linux_function_app :
-    function_app_key => format("https://%s/", function_app.default_hostname)
-  } : null
+  value = module.Azure_Function_App.linux_function_app_default_hostnames
+
 }
 
 output "windows_function_app_default_hostnames" {
   description = "The Default Hostnames associated with the Azure Function Apps for winfow OS"
-  value = os_type == "Windows" ? {
-    for function_app_key, function_app in azurerm_windows_function_app.window_function_app :
-    function_app_key => format("https://%s/", function_app.default_hostname)
-  } : null
+  value = module.Azure_Function_App.windows_function_app_default_hostnames
+   
 }
